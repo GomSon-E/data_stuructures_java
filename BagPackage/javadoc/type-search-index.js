eebf8eaa37b9a1f440689e8cb3b4de6e8b7f2720
Gomsonixx <gomsonixx@gmail.com>
2021-10-31 19:06:16 +0900

    Add Bag

---
+++ BagPackage/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"BagPackage","l":"ArrayBag"},{"p":"BagPackage","l":"BagInterface"},{"p":"BagPackage","l":"LinkedBag"}];updateSearchResults();
